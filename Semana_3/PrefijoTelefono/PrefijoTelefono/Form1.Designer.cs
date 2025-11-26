74378649dc5bd3dd34569757ddef4c8da7451c4a
Jean Holguin <holguinjean1@gmail.com>
2025-11-25 22:01:54 -0400

    Commit proyecto Prefijo Telefono

---
+++ Semana_3/PrefijoTelefono/PrefijoTelefono/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace PrefijoTelefono {
+	partial class Form1 {
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		private System.Windows.Forms.DataGridView dataGridView1;
+		private System.Windows.Forms.Button btnCargarContactos;
+		private System.Windows.Forms.TextBox txtPrefijo;
+		private System.Windows.Forms.Button btnBuscarPrefijo;
+		private System.Windows.Forms.Label label1; // Para la etiqueta del prefijo
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing) {
+			if (disposing && (components != null)) {
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent() {
+			this.dataGridView1 = new System.Windows.Forms.DataGridView();
+			this.btnCargarContactos = new System.Windows.Forms.Button();
+			this.txtPrefijo = new System.Windows.Forms.TextBox();
+			this.btnBuscarPrefijo = new System.Windows.Forms.Button();
+			this.label1 = new System.Windows.Forms.Label();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// dataGridView1
+			// 
+			this.dataGridView1.AllowUserToAddRows = false;
+			this.dataGridView1.AllowUserToDeleteRows = false;
+			this.dataGridView1.AllowUserToResizeColumns = false;
+			this.dataGridView1.AllowUserToResizeRows = false;
+			this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dataGridView1.Location = new System.Drawing.Point(12, 50);
+			this.dataGridView1.Name = "dataGridView1";
+			this.dataGridView1.Size = new System.Drawing.Size(776, 388);
+			this.dataGridView1.TabIndex = 0;
+			// 
+			// btnCargarContactos
+			// 
+			this.btnCargarContactos.Location = new System.Drawing.Point(12, 12);
+			this.btnCargarContactos.Name = "btnCargarContactos";
+			this.btnCargarContactos.Size = new System.Drawing.Size(120, 23);
+			this.btnCargarContactos.TabIndex = 1;
+			this.btnCargarContactos.Text = "Cargar Todos";
+			this.btnCargarContactos.UseVisualStyleBackColor = true;
+			this.btnCargarContactos.Click += new System.EventHandler(this.btnCargarContactos_Click);
+			// 
+			// txtPrefijo
+			// 
+			this.txtPrefijo.Location = new System.Drawing.Point(280, 14);
+			this.txtPrefijo.Name = "txtPrefijo";
+			this.txtPrefijo.Size = new System.Drawing.Size(100, 20);
+			this.txtPrefijo.TabIndex = 2;
+			// 
+			// btnBuscarPrefijo
+			// 
+			this.btnBuscarPrefijo.Location = new System.Drawing.Point(386, 12);
+			this.btnBuscarPrefijo.Name = "btnBuscarPrefijo";
+			this.btnBuscarPrefijo.Size = new System.Drawing.Size(120, 23);
+			this.btnBuscarPrefijo.TabIndex = 3;
+			this.btnBuscarPrefijo.Text = "Buscar por Prefijo";
+			this.btnBuscarPrefijo.UseVisualStyleBackColor = true;
+			this.btnBuscarPrefijo.Click += new System.EventHandler(this.btnBuscarPrefijo_Click);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(170, 17);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(99, 13);
+			this.label1.TabIndex = 4;
+			this.label1.Text = "Prefijo de Teléfono:";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.btnBuscarPrefijo);
+			this.Controls.Add(this.txtPrefijo);
+			this.Controls.Add(this.btnCargarContactos);
+			this.Controls.Add(this.dataGridView1);
+			this.Name = "Form1";
+			this.Text = "Gestión de Contactos";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+		#endregion
+
+		private System.Windows.Forms.GroupBox groupBox1;
+		private System.Windows.Forms.Button button1;
+	}
+}
+
---
+++ Semana_3/PrefijoTelefono/PrefijoTelefono/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace PrefijoTelefono {
+	public partial class Form1 : Form {
+
+		private string connectionString = "Data Source=JEAN_HOLGUIN;Initial Catalog=PrefijoTelefono;Integrated Security=True";
+
+		public Form1() {
+			InitializeComponent();
+			dataGridView1.AutoGenerateColumns = true;
+		}
+
+		private void Form1_Load(object sender, EventArgs e) {
+			// Opcional: Cargar datos al iniciar el formulario
+			CargarDatosContactos();
+		}
+
+		private void CargarDatosContactos(string prefijoTelefono = null) {
+			// Usamos 'using' para asegurar que los recursos (conexión, comando, lector) se cierren y liberen correctamente.
+			using (SqlConnection connection = new SqlConnection(connectionString)) {
+				try {
+					connection.Open(); // Abrir la conexión a la base de datos
+
+					// Crear un comando para ejecutar el procedimiento almacenado
+					using (SqlCommand command = new SqlCommand("sprodObtenerTlfno", connection)) {
+						command.CommandType = CommandType.StoredProcedure; // Indicar que es un procedimiento almacenado
+
+						// Si se proporciona un prefijo, añadirlo como parámetro al procedimiento almacenado
+						if (prefijoTelefono != null) {
+							command.Parameters.AddWithValue("@prefijo", prefijoTelefono);
+						} else {
+							// Si no hay prefijo, el procedimiento almacenado usará su valor por defecto (NULL)
+							// o puedes añadir un parámetro DBNull.Value explícitamente:
+							command.Parameters.AddWithValue("@prefijo", DBNull.Value);
+						}
+
+						// Crear un adaptador de datos para llenar un DataTable
+						using (SqlDataAdapter adapter = new SqlDataAdapter(command)) {
+							DataTable dataTable = new DataTable(); // Crear una tabla de datos
+							adapter.Fill(dataTable); // Llenar la tabla de datos con los resultados del comando
+
+							// Asignar la tabla de datos como la fuente de datos del DataGridView
+							dataGridView1.DataSource = dataTable;
+						}
+					}
+				} catch (Exception ex) {
+					// Manejo de errores: Mostrar un mensaje al usuario si algo sale mal
+					MessageBox.Show("Error al cargar los datos: " + ex.Message, "Error de Conexión", MessageBoxButtons.OK, MessageBoxIcon.Error);
+				}
+			}
+		}
+
+		// Evento Click del botón "Cargar Contactos" (asegúrate de tener un botón llamado 'btnCargarContactos' en tu formulario)
+		private void btnCargarContactos_Click(object sender, EventArgs e) {
+			CargarDatosContactos(); // Llamar al método para cargar todos los contactos
+		}
+
+		// Evento Click del botón "Buscar por Prefijo" (asegúrate de tener un botón llamado 'btnBuscarPrefijo' y un TextBox 'txtPrefijo' en tu formulario)
+		private void btnBuscarPrefijo_Click(object sender, EventArgs e) {
+			// Obtener el prefijo del TextBox
+			string prefijo = txtPrefijo.Text.Trim();
+			CargarDatosContactos(prefijo); // Cargar contactos filtrados por el prefijo
+		}
+	}
+}
